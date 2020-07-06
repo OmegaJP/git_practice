1d2b9b3a50332935e7771636da24fa6822c08a89
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-06 17:03:02 -0500

    Create C_02_expressions.py
    current practice on python.

---
+++ C_02_expressions.py
@@ -0,0 +1,25 @@
+python
+print(4)
+type('Hello World!')
+type(5)
+type(7.9)
+n=17
+print(n)
+
+#First exercise: assign a variable and see what it does
+
+x=5
+y=x+1
+print (y)
+
+# quotients and remainders
+
+quotients=n//4
+print(quotients)
+remainders=n%4
+print(remainders)
+
+# practicing with input
+
+name=input('What is your name?\n')
+print('Hello', name)
