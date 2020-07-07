92731e5df7c3b8aa0e88b6792a579b943e04db73
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-07 15:35:30 -0500

    Create Ex_05_C2.py
    New script that converts celsius degrees to fahrenheit degrees.

---
+++ python_scripts/Ex_05_C2.py
@@ -0,0 +1,5 @@
+#Python script that changes Celsius degrees to Fahrenheit
+
+Celsius=input('Enter degrees Celsius: ')
+Fahrenheit= 9/5*float(Celsius) + 32
+print('degrees Fahrenheit:', Fahrenheit,  'F')
