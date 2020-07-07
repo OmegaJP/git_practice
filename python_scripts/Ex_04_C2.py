5e796d6120fe1c36454221856b725996cb92190c
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-07 14:03:36 -0500

    added new script exercise

---
+++ python_scripts/Ex_04_C2.py
@@ -0,0 +1,23 @@
+#Assigning variables
+width=17
+height=12.0
+
+#Doing first operation
+a=width/2
+print(a)
+print(type(a))
+
+#second operation
+b=width/2.0
+print(b)
+print(type(b))
+
+#third operation
+c=height/3
+print(c)
+print(type(c))
+
+#fourth operation
+d=1+2*5
+print(d)
+print(type(d))
