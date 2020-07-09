3dc3ce4f8aa2f14878c4075a8608e145386a7fde
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-09 16:00:26 -0500

    change of directory

--- python_scripts/Ex_03_C2.py
+++
@@ -1,4 +0,0 @@
-h=input('Enter hours: ', )
-r=input('Enter rate: ', )
-p=float(h)*float(r)
-print('Your pay is:', p)
--- python_scripts/Ex_05_C2.py
+++
@@ -1,5 +0,0 @@
-#Python script that changes Celsius degrees to Fahrenheit
-
-Celsius=input('Enter degrees Celsius: ')
-Fahrenheit= 9/5*float(Celsius) + 32
-print('degrees Fahrenheit:', Fahrenheit,  'F')
