3cd200b39ed2e78c723304e98b2b189a4da0e233
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-09 15:56:14 -0500

    add new folders and a new file

---
+++ python_scripts/Chapter02/C_02_expressions.py
@@ -0,0 +1,27 @@
+print(4)
+print(type(4))
+print('Hello World!')
+print(type('Hello World!'))
+print(7.9)
+print(type(7.9))
+n=17
+print(n)
+
+#First exercise: assign a variable and see what it does
+
+x=5
+y=x+1
+print('x =', x)
+print ('y =', y)
+
+# quotients and remainders
+
+quotients=n//4
+print(quotients)
+remainders=n%4
+print(remainders)
+
+# practicing with input
+
+name=input('What is your name?\n')
+print('Hello', name)
