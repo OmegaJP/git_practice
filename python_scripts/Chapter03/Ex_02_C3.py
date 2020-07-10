4b4bd431917623cdba2ca582444fe0bf662bdbcb
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-10 09:37:47 -0500

    correction

--- python_scripts/Chapter03/Ex_02_C3.py
+++ python_scripts/Chapter03/Ex_02_C3.py
@@ -1,11 +1,11 @@
 hours= input('Enter the working hours: ')
 rate=input('Enter pay rate: ')
 try :
-if float(hours) > 40 :
-    pay= float(hours)*float(rate)*1.5
-else :
-    pay= float(hours)*float(rate)
-print('Your pay is of: ', pay)
+    if float(hours) > 40 :
+        pay= float(hours)*float(rate)*1.5
+    else :
+        pay= float(hours)*float(rate)
+        print('Your pay is of: ', pay)
 except :
 print('Please enter a correct number')
 quit()
