83c032c07dca5d65ce5d057d96bb7e26bd3bd16e
OmegaJP <36650978+OmegaJP@users.noreply.github.com>
2020-07-15 12:41:03 -0500

    added a counter to the script

--- python_scripts/Chapter06/Ex_01_C6.py
+++ python_scripts/Chapter06/Ex_01_C6.py
@@ -1,9 +1,12 @@
-#script that reads a word backwards and list each letter of the word. 
+#script that reads a word backwards and list each letter of the word.
 
 word=input('Please enter word: ')
+n=0
 index=len(word)-1
 while index < -1 :
     letter=word[index]
     print(letter)
     index=index-1
+    n=n+1
 print(word)
+print('count: ', n)
